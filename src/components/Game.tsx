3ad28bc2cbefe303d0d7bac70d70ddce5c4daf43
Alexander Ho <ho.alexander.g@gmail.com>
2022-09-25 07:47:16 +0800

    Added check winner

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,45 @@
-import React, { MouseEvent, useState } from 'react';
+import React, { MouseEvent, useState, useEffect } from 'react';
 import { Grid, GridItem, Flex, AspectRatio, Image } from '@chakra-ui/react';
 import xIcon from '../assets/x-icon.png';
 import oIcon from '../assets/o-icon.png';
 
+enum Winner {
+  NONE,
+  PLAYERX,
+  PLAYERO,
+}
+
 const Game = () => {
   const bgColor = 'theme.background';
+  const WINNING_PATTERN = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
   const [cells, setCells] = useState<Array<typeof xIcon | typeof oIcon | ''>>(Array(9).fill(''));
   const [isXTurn, setIsXTurn] = useState<boolean>(true);
+  const [whoWon, setWhoWon] = useState<Winner>(Winner.NONE);
+
+  useEffect(() => {
+    if (checkWin(xIcon)) {
+      setWhoWon(Winner.PLAYERX);
+    } else if (checkWin(oIcon)) {
+      setWhoWon(Winner.PLAYERO);
+    }
+  }, [cells]);
+
+  useEffect(() => {
+    console.log(whoWon);
+  }, [whoWon]);
+
+  const checkWin = (playerMarker: typeof xIcon | typeof oIcon | '') =>
+    WINNING_PATTERN.some((pattern) => pattern.every((index) => cells[index] === playerMarker));
 
   const handleClick = (event: MouseEvent<HTMLDivElement>, index: number) => {
     if (cells[index] !== '') return;

bab38dfcac11bf6489a9523e6365abaf7e2e8448
Alexander Ho <ho.alexander.g@gmail.com>
2022-09-25 10:54:16 +0800

    Added pop-up message upon game finishes

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,11 @@
 import React, { MouseEvent, useState, useEffect } from 'react';
-import { Grid, GridItem, Flex, AspectRatio, Image } from '@chakra-ui/react';
+import { Grid, GridItem, Flex, AspectRatio, Image, Box, Button, Img } from '@chakra-ui/react';
 import xIcon from '../assets/x-icon.png';
 import oIcon from '../assets/o-icon.png';
 
 enum Winner {
   NONE,
+  DRAW,
   PLAYERX,
   PLAYERO,
 }
@@ -27,6 +28,10 @@ const Game = () => {
   const [whoWon, setWhoWon] = useState<Winner>(Winner.NONE);
 
   useEffect(() => {
+    if (cells.every((cell) => cell !== '')) {
+      setWhoWon(Winner.DRAW);
+    }
+
     if (checkWin(xIcon)) {
       setWhoWon(Winner.PLAYERX);
     } else if (checkWin(oIcon)) {
@@ -42,7 +47,7 @@ const Game = () => {
     WINNING_PATTERN.some((pattern) => pattern.every((index) => cells[index] === playerMarker));
 
   const handleClick = (event: MouseEvent<HTMLDivElement>, index: number) => {
-    if (cells[index] !== '') return;
+    if (cells[index] !== '' || whoWon !== Winner.NONE) return;
 
     const newCells = [...cells];
 
@@ -55,6 +60,12 @@ const Game = () => {
     }
 
     setCells(newCells);
+  };
+
+  const handleRestart = () => {
+    setCells(Array(9).fill(''));
+    setIsXTurn(true);
+    setWhoWon(Winner.NONE);
   };
 
   return (
@@ -87,6 +98,47 @@ const Game = () => {
           ))}
         </Grid>
       </AspectRatio>
+
+      <Flex
+        display={whoWon !== Winner.NONE ? 'flex' : 'none'}
+        flexDir='column'
+        top='50%'
+        left='50%'
+        transform='translate(-50%, -50%)'
+        w='100%'
+        h='65%'
+        justify='center'
+        align='center'
+        bgColor={whoWon === Winner.PLAYERX ? 'blue.400' : 'red.400'}
+        color={whoWon === Winner.PLAYERX ? 'red.500' : 'blue.500'}
+        position='fixed'
+        fontSize='5rem'
+        fontWeight='600'
+        textAlign='center'
+        userSelect='none'
+      >
+        {whoWon === Winner.PLAYERX ? (
+          <Box>
+            <Img src={xIcon} display='inline' mr='8' h='5rem' userSelect='none' draggable='false' />
+            won
+          </Box>
+        ) : whoWon === Winner.PLAYERO ? (
+          <Box>
+            <Img src={oIcon} display='inline' mr='8' h='5rem' userSelect='none' draggable='false' />
+            won
+          </Box>
+        ) : (
+          'Draw'
+        )}
+
+        <Button
+          onClick={handleRestart}
+          colorScheme={whoWon === Winner.PLAYERX ? 'red' : 'blue'}
+          mt='3%'
+        >
+          Restart
+        </Button>
+      </Flex>
     </Flex>
   );
 };
